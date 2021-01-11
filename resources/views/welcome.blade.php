dffe3313dbf3fad1998f3ffa06c254f76b6d9c4e
Douglas Cockerham <padretres@gmail.com>
2021-01-11 12:20:46 -0500

    Added a typical day

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -115,6 +115,31 @@
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
                 </div>
+
+                <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+                    <div class="grid grid-cols-1 md:grid-cols-2">
+                        <div class="p-6">
+                            <div class="flex items-center">
+{{--                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>--}}
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width=".5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><defs><style>.cls-1{fill:none;stroke-linecap:round;stroke-linejoin:round;stroke-width:2px;}.cls-1,.cls-2{font-size:6px;font-family:Belgium;}.cls-2,.cls-3{fill:#231f20;}.cls-3{stroke-miterlimit:10;stroke-width:0.5px;}</style></defs><rect class="cls-1" x="1" y="1" width="15.72" height="15.72"/><text class="cls-2" transform="translate(7.57 7.5)">14</text><line class="cls-3" x1="4.55" y1="9.74" x2="14.78" y2="9.74"/><line class="cls-3" x1="4.55" y1="11.22" x2="14.78" y2="11.22"/><line class="cls-3" x1="4.55" y1="12.7" x2="14.78" y2="12.7"/><line class="cls-3" x1="4.55" y1="14.18" x2="14.78" y2="14.18"/></svg>
+                                <div class="ml-4 text-lg leading-7 font-semibold text-gray-500">A typical day goes something like this:</div>
+                            </div>
+
+                            <div class="ml-12">
+                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rJGhA2vLdA4" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/TKQyPfN6s88" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pebaBnA6PTY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/YDXduFEjiQk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/II1x9ptMZag" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
             </div>
         </div>
     </body>
