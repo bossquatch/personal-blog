dd8bb9a31063c061d88dd0b2b50176907b24133d
Douglas Cockerham <padretres@gmail.com>
2021-01-27 10:55:46 -0500

    added stallions

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -72,12 +72,12 @@
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                             <div class="flex items-center">
                                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://foxnews.com/" class="underline text-gray-900 dark:text-white">Latest News</a></div>
+                                <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://foxnews.com/" class="underline text-gray-900 dark:text-white">My Team</a></div>
                             </div>
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    I can't wait to fill this in!
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/9tOlTDS10gI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                                 </div>
                             </div>
                         </div>
