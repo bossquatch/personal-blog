76e94f14069aa2fe369cf2baf0d8113e7e489476
Douglas Cockerham <padretres@gmail.com>
2021-02-01 13:52:37 -0500

    asdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -108,7 +108,7 @@
                         </div>
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-l">
                             <div class="flex items-center">
-                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                                <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 160.29 202.7" width="24"><rect x="21.06" y="1" width="18.95" height="200.7" style="fill:none;stroke:#a0aec0;stroke-miterlimit:10;stroke-width:2px"/><polyline points="21.06 41.02 1 41.02 1 22.06 21.06 22.06" style="fill:none;stroke:#a0aec0;stroke-miterlimit:10;stroke-width:2px"/><polyline points="40.02 22.06 159.29 22.06 159.29 41.02 40.02 41.02" style="fill:none;stroke:#a0aec0;stroke-miterlimit:10;stroke-width:2px"/><rect x="49.18" y="47.78" width="101.09" height="67.84" rx="5.65" style="fill:none;stroke:#a0aec0;stroke-miterlimit:10;stroke-width:2px"/></svg>
                                 <div class="ml-4 text-lg leading-7 font-semibold text-gray-900 dark:text-white">This space for rent</div>
                             </div>
 
